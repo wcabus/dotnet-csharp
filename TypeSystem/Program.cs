d7bc6498307edeba36bdf20be076ec2d4cb10133
Wesley Cabus <wesley@gotsharp.be>
2018-01-03 11:13:42 +0100

    Finished the type system sample code

--- TypeSystem/Program.cs
+++ TypeSystem/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace TypeSystem
 {
     class Program
     {
+        [Test]
         static void Main(string[] args)
         {
             var pointRef = new Point();
@@ -21,6 +23,50 @@ namespace TypeSystem
             pointVal2.Y = 2;
 
             Console.WriteLine($"Point 1: {pointVal}, Point 2: {pointVal2}");
+            
+            // This next line will throw an exception at run-time
+            // ReadAttributes();
+        }
+
+        static void ReadAttributes()
+        {
+            var attribute = typeof(Program).GetMethod(nameof(Main)).GetCustomAttributes(typeof(TestAttribute), false)
+                .OfType<TestAttribute>().FirstOrDefault();
+        }
+    }
+
+    public class TestAttribute : Attribute
+    {
+        public TestAttribute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITest
+    {
+        void Add(int data);
+    }
+
+    public class ImplicitTest : ITest
+    {
+        public void Add(int data) { }
+    }
+
+    public class ExplicitTest : ITest
+    {
+        void ITest.Add(int data) { }
+    }
+
+    public class InterfaceDifference
+    {
+        public void Test()
+        {
+            var implicitInstance = new ImplicitTest();
+            var explicitInstance = new ExplicitTest();
+
+            implicitInstance.Add(1);
+            ((ITest)explicitInstance).Add(2);
         }
     }
 }
