c515f418c98b6e5f4ecadbf8e1bf04c3a94f112f
Wesley Cabus <wesley@gotsharp.be>
2018-01-03 10:59:41 +0100

    Added first sample

---
+++ TypeSystem/Point.cs
@@ -0,0 +1,41 @@
+﻿namespace TypeSystem
+{
+    public class Point
+    {
+        public Point() : this(0, 0)
+        {
+            
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+
+    public struct Point2D
+    {
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ TypeSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TypeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pointRef = new Point();
+            var pointRef2 = pointRef;
+
+            pointRef.X = 5;
+            pointRef2.Y = 2;
+
+            Console.WriteLine($"Point 1: {pointRef}, Point 2: {pointRef2}");
+
+            var pointVal = new Point2D();
+            var pointVal2 = pointVal;
+
+            pointVal.X = 5;
+            pointVal2.Y = 2;
+
+            Console.WriteLine($"Point 1: {pointVal}, Point 2: {pointVal2}");
+        }
+    }
+}
