53dee8bde1e0443316504e88a80386f13632962d
Wesley Cabus <wesley@gotsharp.be>
2018-01-04 16:09:30 +0100

    Added threading samples

---
+++ Threading/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadingSample
+{
+    class Program
+    {
+        private static bool _stopRunning;
+
+        static void Main()
+        {
+            Console.WriteLine("Press any key to start running the sample...");
+            Console.ReadKey(true);
+
+            try
+            {
+                // ReadNumbersAndAddThem();
+                // ForegroundBlockingThread();
+                // BackgroundBlockingThread();
+            }
+            finally
+            {
+                // When you run this app with CTRL+F5 (start without debugging), it pauses at the end.
+                // The normal behavior though, is to run and exit immediately, so I added these lines when the debugger is attached.
+                if (Debugger.IsAttached)
+                {
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey(true);
+                }
+            }
+        }
+
+        static void ReadNumbersAndAddThem()
+        {
+            Console.WriteLine("Keep entering numbers and confirm them with <ENTER> to add them.");
+            Console.WriteLine("To stop, press <ENTER> on an empty line.");
+
+            var queue = new Queue<int>();
+            string input;
+
+            var adderThread = new Thread(NumberAdderThread)
+            {
+                Name = "Adds numbers",
+                IsBackground = true // Default = false when manually creating new threads this way
+            };
+                
+            adderThread.Start(queue);
+
+            while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+            {
+                if (int.TryParse(input, out var number))
+                {
+                    queue.Enqueue(number);
+                }
+            }
+
+            _stopRunning = true;
+        }
+
+        static void NumberAdderThread(object state)
+        {
+            var queue = state as Queue<int>;
+            var total = 0;
+
+            if (queue == null)
+            {
+                return; 
+            }
+
+            while (!_stopRunning)
+            {
+                if (queue.Count > 0)
+                {
+                    while (queue.Count > 0)
+                    {
+                        var number = queue.Dequeue();
+                        total += number;
+                    }
+
+                    Console.WriteLine($"Total of all entered numbers: {total}");
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void ForegroundBlockingThread()
+        {
+            new Thread(() =>
+                {
+                    while (!_stopRunning)
+                    {
+                        Console.WriteLine("Thread is still running");
+                        Thread.Sleep(1000);
+                    }
+                })
+                {
+                    // IsBackground = false // A foreground thread
+                }
+                .Start();
+        }
+
+        static void BackgroundBlockingThread()
+        {
+            new Thread(() =>
+                {
+                    while (!_stopRunning)
+                    {
+                        Console.WriteLine("Thread is still running");
+                        Thread.Sleep(1000);
+                    }
+                })
+                {
+                    IsBackground = true // A background thread
+                }
+                .Start();
+        }
+    }
+}
